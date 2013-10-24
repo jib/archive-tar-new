bf5156b22d6efc6ae243d6ff77c21d1b98f77622
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2013-10-24 18:59:43 +0100

    Update CHANGES, bump to version 1.94

--- lib/Archive/Tar/Constant.pm
+++ lib/Archive/Tar/Constant.pm
@@ -3,7 +3,7 @@ package Archive::Tar::Constant;
 BEGIN {
     require Exporter;
 
-    $VERSION    = '1.93_02';
+    $VERSION    = '1.94';
     @ISA        = qw[Exporter];
 
     require Time::Local if $^O eq "MacOS";
