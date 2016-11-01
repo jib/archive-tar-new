c12f33da0b009b4080cbf7eaa2b3fb36593f3759
Chris 'BinGOs' Williams <chris@bingosnet.co.uk>
2016-11-01 19:18:53 +0000

    Release engineering for 2.16

--- lib/Archive/Tar/Constant.pm
+++ lib/Archive/Tar/Constant.pm
@@ -3,7 +3,7 @@ package Archive::Tar::Constant;
 BEGIN {
     require Exporter;
 
-    $VERSION    = '2.14';
+    $VERSION    = '2.16';
     @ISA        = qw[Exporter];
 
     require Time::Local if $^O eq "MacOS";
