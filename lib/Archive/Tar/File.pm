jib/archive-tar-new

commit a00e0305c16256c6f5353ac3e16ec854d2c0c238

use canonpath in _prefix_and_file

lib/Archive/Tar/File.pm
@@ -396,12 +396,7 @@ sub _prefix_and_file {
     my $path = shift;
 
     my ($vol, $dirs, $file) = File::Spec->splitpath( $path, $self->is_dir );
-    my @dirs = File::Spec->splitdir( $dirs );
-
-    ### so sometimes the last element is '' -- probably when trailing
-    ### dir slashes are encountered... this is of course pointless,
-    ### so remove it
-    pop @dirs while @dirs and not length $dirs[-1];
+    my @dirs = File::Spec->splitdir( File::Spec->canonpath($dirs) );
 
     ### if it's a directory, then $file might be empty
     $file = pop @dirs if $self->is_dir and not length $file;
@@ -409,9 +404,7 @@ sub _prefix_and_file {
     ### splitting ../ gives you the relative path in native syntax
     map { $_ = '..' if $_  eq '-' } @dirs if ON_VMS;
 
-    my $prefix = File::Spec::Unix->catdir(
-                        grep { length } $vol, @dirs
-                    );
+    my $prefix = File::Spec::Unix->catdir(@dirs);
     return( $prefix, $file );
 }
 

commit 065d18c0aa195c79f7131b425e3060cac7944c21

Fix absolute path handling on VMS.
a00e0305c16256c6f broke absolute path handling on filesystems that
have the concept of a volume because it discarded the volume
returned from splitpath.  This restores that on VMS only.

Also, to get the absolute path test added in 53dac726398c2c08
working on VMS, it's necessary to remove the root directory name
(000000) and ensure all path components are in Unix format.

lib/Archive/Tar/File.pm
@@ -402,7 +402,15 @@ sub _prefix_and_file {
     $file = pop @dirs if $self->is_dir and not length $file;
 
     ### splitting ../ gives you the relative path in native syntax
-    map { $_ = '..' if $_  eq '-' } @dirs if ON_VMS;
+    ### Remove the root (000000) directory
+    ### The volume from splitpath will also be in native syntax
+    if (ON_VMS) {
+        map { $_ = '..' if $_  eq '-'; $_ = '' if $_ eq '000000' } @dirs;
+        if (length($vol)) {
+            $vol = VMS::Filespec::unixify($vol);
+            unshift @dirs, $vol;
+        }
+    }
 
     my $prefix = File::Spec::Unix->catdir(@dirs);
     return( $prefix, $file );