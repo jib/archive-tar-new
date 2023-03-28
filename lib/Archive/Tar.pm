935908bc1c60e1d7132b6cb5ed207608a9bdd5ab
Manfred Stock <mstock@cpan.org>
2023-03-28 17:17:35 +0200

    Improve formatting of $Archive::Tar::RESOLVE_SYMLINK documentation
    The lines in this paragraph were indented which tended to make it
    being rendered as code block which looked bad in both text based
    POD viewers as well as when this was rendered as HTML.

--- lib/Archive/Tar.pm
+++ lib/Archive/Tar.pm
@@ -2153,25 +2153,31 @@ numbers. Added for compatibility with C<busybox> implementations.
 
 =head2 Tuning the way RESOLVE_SYMLINK will works
 
-	You can tune the behaviour by setting the $Archive::Tar::RESOLVE_SYMLINK variable,
-	or $ENV{PERL5_AT_RESOLVE_SYMLINK} before loading the module Archive::Tar.
+You can tune the behaviour by setting the $Archive::Tar::RESOLVE_SYMLINK variable,
+or $ENV{PERL5_AT_RESOLVE_SYMLINK} before loading the module Archive::Tar.
 
-  Values can be one of the following:
+Values can be one of the following:
 
-		none
-           Disable this mechanism and failed as it was in previous version (<1.88)
+=over 4
 
-		speed (default)
-           If you prefer speed
-           this will read again the whole archive using read() so all entries
-           will be available
+=item none
 
-    memory
-           If you prefer memory
+Disable this mechanism and failed as it was in previous version (<1.88)
 
-	Limitation
+=item speed (default)
 
-		It won't work for terminal, pipe or sockets or every non seekable source.
+If you prefer speed
+this will read again the whole archive using read() so all entries
+will be available
+
+=item memory
+
+If you prefer memory
+
+=back
+
+Limitation: It won't work for terminal, pipe or sockets or every non seekable
+source.
 
 =head2 $Archive::Tar::EXTRACT_BLOCK_SIZE
 
